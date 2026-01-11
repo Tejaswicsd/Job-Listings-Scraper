7eeb589e32a3d93a35a479b0f8c022c8c2a9a12b
Tejaswicsd <tejaswiguttula1@gmail.com>
2026-01-11 23:24:32 +0530

    Refactor code structure for improved readability and maintainability

---
+++ SRC/JobScraper.java
@@ -0,0 +1,27 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class JobScraper {
+
+    public static void main(String[] args) throws Exception {
+
+        String url = "https://realpython.github.io/fake-jobs/";
+
+        Document doc = Jsoup.connect(url).get();
+
+        Elements jobs = doc.select("div.card-content");
+
+        for (Element job : jobs) {
+            String title = job.select("h2.title").text();
+            String company = job.select("h3.company").text();
+            String location = job.select("p.location").text();
+
+            System.out.println(title);
+            System.out.println(company);
+            System.out.println(location);
+            System.out.println("---------------------");
+        }
+    }
+}
